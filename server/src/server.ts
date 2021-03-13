a5b871f07d4ff9d37d33eefbbf6b7d6069a19cab
Guilherme <guilhermegalli5@gmail.com>
2021-03-13 12:08:14 -0300

    fix deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,9 +7,13 @@ const app = express();
 
 app.use(cors());
 
+app.get('/', (req, res) => {
+	res.send("Hello World!")
+})
+
 app.use(express.json());
 app.use(routes);
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
 
